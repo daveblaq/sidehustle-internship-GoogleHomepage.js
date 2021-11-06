82e03d9e6b6343661702ebdc434d7b26969e203d
kvngdavid <davidoladele80@gmail.com>
2021-11-06 16:19:32 +0100

    snapshot

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 
    
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import logo from '../images/sh-logo-dark.png';
 
@@ -118,13 +118,21 @@ const MicIcon = styled.div`
 `;
 
 const Home = () => {
+	const [textEntered, setTextEntered] = useState(logo);
+
+	const textHandler = (e) => {
+		 setTextEntered(e.target.value);
+
+		
+	}
   return (
     <Container>
       <LogoSection>
-        <img
+		  {textEntered ? <p>{textEntered}</p> : <img
           src={logo}
           alt='Sidehustle-logo'
-        />
+        /> }
+        
       </LogoSection>
       <SearchSection>
         <Form action='/' method='GET' role='search'>
@@ -138,7 +146,7 @@ const Home = () => {
                 <path d='M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z' />
               </svg>
             </SearchIcon>
-            <SearchInput type='text' />
+            <SearchInput type='text' placeholder='Enter some text' onChange={textHandler} />
             <MicIcon>
               <svg
                 focusable='false'
